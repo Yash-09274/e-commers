611c2beed4fde73caa427c76858c34a4858e094f
yash vaish <148077975+Yash-09274@users.noreply.github.com>
2024-01-27 22:50:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const product = [
+    {id:1 ,name : "product" , Price:20}
+    {id :2, name :"product 2",price:30}
+
+]
+
+let cart=[];
+function addtocart(){
+    constproductid=1;
+    constproductid=productsfind <p, id===productid>
+
+    if(product){
+        cart.push(product);
+        updatecart();
+    }
+}
+
+function updatecart(){
+    const cartitemcontainer=document.getElementById(cartitem)
+    cartitemcontainer.innerHTML=' ';;
+    cartforeachitem => {
+        const cartitem=document.createElement('div');
+        const item.innerHTML=<P> {itemname} -{$$itemprice} </P>;
+    }
+}
+function checkout(){
+    window.location.href='otp.html'
+}
+function verifyotp(){
+    alert(otpverified);
+}
